7d3c9fb8d2d69aa00a3f52089ece4fb11bfd50c4
Mtang <mifzal1312@gmail.com>
2026-07-01 16:59:11 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,19 @@
+from sklearn.linear_model import LinearRegression
+
+
+X = [[1], [2], [3], [4], [5]]
+
+
+y = [20, 40, 60, 80, 100]
+
+
+model = LinearRegression()
+
+model.fit(X, y)
+print("Model has successfully learned the data!")
+
+new_student_hours = [[9]]
+prediction = model.predict(new_student_hours)
+
+print(
+    f"If a student studies for {new_student_hours} hours, the AI predicts a score of: {prediction[0]}")
